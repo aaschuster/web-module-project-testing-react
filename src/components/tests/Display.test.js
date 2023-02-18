93fbbb0c5b2616b65920a2c7432c27cc784e2a64
Aaron Schuster <aaronjschuster@gmail.com>
2023-02-18 16:52:41 -0500

    build out final 2 tests for Display.test.js

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -15,11 +15,34 @@ test('renders without errors with no props', async () => {
 test('renders Show component when the button is clicked ', async () => {
 
     mockFetch.mockResolvedValueOnce(egShowData);
-    render(<Display/>);;
+    render(<Display/>);
     userEvent.click(screen.getByRole("button"));
     
     const show = await screen.findByTestId("show-container");
     expect(show).toBeInTheDocument();
 });
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders show season options matching your data when the button is clicked', async () => {
+    
+    mockFetch.mockResolvedValueOnce(egShowData);
+    render(<Display/>);
+    userEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => {
+        const options = screen.queryAllByTestId("season-option")
+        expect(options).toHaveLength(9);
+    })
+    
+});
+
+test("functional prop is called when fetch button is click", async () => {
+    const mockDisplayFunc = jest.fn();
+
+    mockFetch.mockResolvedValueOnce(egShowData);
+    render(<Display displayFunc = {mockDisplayFunc}/>);
+    userEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => {
+        expect(mockDisplayFunc).toBeCalled();
+    })
+});
